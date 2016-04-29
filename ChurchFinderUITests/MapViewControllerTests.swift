ad00b99a4ef73936593973b5b920f2b290a30fbd
Kinkster <mitchellde1@gcc.edu>
2016-04-29 00:44:58 -0400

    New tests and added comments

--- ChurchFinderUITests/MapViewControllerTests.swift
+++ ChurchFinderUITests/MapViewControllerTests.swift
@@ -15,21 +15,15 @@ class MapViewControllerTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
         XCUIApplication().navigationBars.buttons["Map"].tap()
+        // All tests fail if the app doesn't load in time, adjust sleep or try again.
+        // Tests in this file are LOCATION DEPENDANT, will only work in Grove City area
         sleep(20)
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
